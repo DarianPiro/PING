8fa950bc11a20c1c98285dace3349f05d927cc3b
Darian Pirowhedayati <d.pirowhedayati@gmail.com>
2023-03-06 21:02:15 +0100

    Added alternative video files

---
+++ client/src/components/HelperVideoAR.js
@@ -0,0 +1,67 @@
+import React, { useContext, useEffect, useRef } from 'react';
+import { Context } from '../Context';
+import { Atrament } from 'atrament';
+
+const VideoChat = () => {
+  const {
+    call,
+    localVideo,
+    remoteVideo,
+    leaveCall,
+    setStroke,
+    stream,
+  } = useContext(Context);
+
+  const canvasRef = useRef(null);
+  let videoWidth = 600;
+  let videoHeight = 450;
+
+  useEffect(() => {
+    localVideo.current.srcObject = stream;
+    const canvas = canvasRef.current;
+    canvas.width = videoWidth;
+    canvas.height = videoHeight;
+
+    const sketchpad = new Atrament(canvasRef.current, {
+      color: 'orange',
+    });
+
+    sketchpad.recordStrokes = true;
+    sketchpad.smoothing = 1.3;
+    sketchpad.addEventListener('strokerecorded', ({ stroke }) =>
+      setStroke(stroke)
+    );
+  }, [call]);
+
+  return (
+    <div>
+      <div className="video-container" style={{ videoWidth }}>
+        {call.accepted && !call.ended && (
+          <button className="button end-call" onClick={leaveCall}>
+            End Call
+          </button>
+        )}
+        <video
+          className="small-video"
+          playsInline
+          muted
+          ref={localVideo}
+          autoPlay
+          style={{ width: '150px' }}
+        />
+
+        <canvas ref={canvasRef} id="sketchpad" className="sketchpad" />
+        <video
+          className="big-video"
+          playsInline
+          muted
+          ref={remoteVideo}
+          autoPlay
+          style={{ width: videoWidth, height: videoHeight }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default VideoChat;
--- server/models/index.js
+++ server/models/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 try {
   mongoose.connect(
-    "mongodb+srv://P1ro:oDOaxYB8Du3Y9GJx@piro.f8cbqeh.mongodb.net/?retryWrites=true&w=majority",  
+    "mongodb+srv://P1ro:oDOaxYB8Du3Y9GJx@piro.f8cbqeh.mongodb.net/ping?retryWrites=true&w=majority",  
   console.log('💗 Connected to database 💗')
   );
 } catch (error) {
