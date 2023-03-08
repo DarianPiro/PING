a164783d2ec43f9925295e0f7ef6d512d5d88921
Darian Pirowhedayati <d.pirowhedayati@gmail.com>
2023-03-08 01:31:18 +0100

    test

--- server/index.js
+++ server/index.js
@@ -56,6 +56,7 @@ io.on('connection', (socket) => {
 
   socket.on('callUser', ({ userToCall, signalData, from, name }) => {
     io.to(userToCall).emit('callUser', { signal: signalData, from, name });
+    console.log('callUser', userToCall, signalData, from, name);
   });
 
   socket.on('answerCall', (data) => {
